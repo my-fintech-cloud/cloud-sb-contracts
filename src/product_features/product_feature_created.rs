18ac4307a010bd34cffd19c17ede6b1d2816cc70
RsSvc <wonderhays@gmail.com>
2023-07-12 12:34:55 +0300

    sb fixes

--- src/product_features/product_feature_created.rs
+++ src/product_features/product_feature_created.rs
@@ -1,16 +1,31 @@
-pub struct ProductFeatureEnabledSbModel{
+#[derive(Clone, PartialEq, ::prost::Message)]
+#[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "product-feature-enabled")]
+pub struct ProductFeatureEnabledSbModel {
+    #[prost(string, tag = "1")]
     pub process_id: String,
+    #[prost(string, tag = "2")]
     pub tenant_id: String,
+    #[prost(string, tag = "3")]
     pub product_id: String,
+    #[prost(string, tag = "4")]
     pub product_instance_id: String,
+    #[prost(string, tag = "5")]
     pub feature_id: String,
+    #[prost(string, tag = "6")]
     pub feature_instance_id: String,
 }
 
-pub struct ProductFeatureDeployedSbModel{
+#[derive(Clone, PartialEq, ::prost::Message)]
+#[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "product-feature-deployed")]
+pub struct ProductFeatureDeployedSbModel {
+    #[prost(string, tag = "1")]
     pub tenant_id: String,
+    #[prost(string, tag = "2")]
     pub product_id: String,
+    #[prost(string, tag = "3")]
     pub product_instance_id: String,
+    #[prost(string, tag = "4")]
     pub feature_id: String,
+    #[prost(string, tag = "5")]
     pub feature_instance_id: String,
 }
--- src/tenant_products/tenant_product_created.rs
+++ src/tenant_products/tenant_product_created.rs
@@ -1,13 +1,25 @@
-pub struct AddTenantProductSbModel{
+#[derive(Clone, PartialEq, ::prost::Message)]
+#[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "add-tenant-product")]
+pub struct AddTenantProductSbModel {
+    #[prost(string, tag = "1")]
     pub process_id: String,
+    #[prost(string, tag = "2")]
     pub tenant_id: String,
+    #[prost(string, tag = "3")]
     pub product_id: String,
+    #[prost(string, tag = "4")]
     pub product_instance_id: String,
 }
 
-pub struct TenantProductAddedSbModel{
+#[derive(Clone, PartialEq, ::prost::Message)]
+#[my_service_bus_macros::my_sb_entity_protobuf_model(topic_id = "tenant-product-added")]
+pub struct TenantProductAddedSbModel {
+    #[prost(string, tag = "1")]
     pub tenant_id: String,
+    #[prost(string, tag = "2")]
     pub product_id: String,
+    #[prost(string, tag = "3")]
     pub product_instance_id: String,
+    #[prost(string, tag = "4")]
     pub ip: String,
 }
